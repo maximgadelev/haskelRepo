71eff6abf2444f35ba07853c4ca98e59dcee4b6c
maximgadelev <maximgadelev2@gmail.com>
2023-11-24 13:31:41 +0300

    hw

---
+++ hw2/src/MyLib.hs
@@ -0,0 +1,51 @@
+module MyLib where
+
+rev :: [a] -> [a]
+rev [] = []
+rev (x:xs) = xs -- [x]
+
+-- Binary Search Tree
+--
+-- left < root <= right
+data Tree a
+  = Empty
+  | Node
+    { left :: Maybe (Tree a)
+    , value :: a
+    , right :: Maybe (Tree a)
+    }
+  deriving (Eq,Show,Read)
+
+empty :: Tree a
+empty = Empty
+
+leaf :: a -> Tree a
+leaf a = Node Nothing a Nothing
+
+traversal :: Tree a -> [a]
+traversal Empty = []
+traversal (Node ml v mr)
+  = maybe [] traversal ml ++ [v] ++ maybe [] traversal mr
+
+insert :: Ord a => a -> Tree a -> Tree a
+insert v Empty = leaf v
+insert v t@(Node ml root mr)
+  | v < root  = t{ left = Just $ maybe (leaf v) (insert v) ml }
+  | otherwise = t{ right= Just $ maybe (leaf v) (insert v) mr }
+
+-- Напишите тесты-свойства к функциям и сами функции
+-- левого и правого поворота деревьев
+-- (см. https://en.wikipedia.org/wiki/Red%E2%80%93black_tree)
+rotateLeft :: Tree a -> Tree a
+rotateLeft Empty = Empty
+rotateLeft (Node maybeLeftSubtree value maybeRightSubtree) =
+  case maybeRightSubtree of
+    Nothing -> Empty
+    Just rightSubtree -> Node (Just rightSubtree) value maybeLeftSubtree
+
+rotateRight :: Tree a -> Tree a
+rotateRight Empty = Empty
+rotateRight (Node maybeLeftSubtree value maybeRightSubtree) =
+  case maybeLeftSubtree of
+    Nothing -> Empty
+    Just leftSubtree -> Node maybeRightSubtree value (Just leftSubtree)
